74fb4f84200d7e4b158b39be6f4a91f30612dfd7
Unknown <alejandro_jimenez24@hotmail.com>
2017-11-11 14:00:38 -0600

    Redes - Actualizar Relaciones

--- app/scripts/controllers/catalogos/RedesUpdateCtrl.js
+++ app/scripts/controllers/catalogos/RedesUpdateCtrl.js
@@ -6,6 +6,17 @@ angular
         
         function initData(){
             GetRed();
+            CatalogosRedIPFactory.GetGeneral_Sistema_II().then(function(data){
+                console.log(data);
+                vm.AlcanceRedIP = data.GetGeneral_Sistema_IIResult.AlcanceRedIp;
+                vm.BlockTab1 = (1 <= vm.AlcanceRedIP)? true:false;
+                vm.BlockTab2 = (2 <= vm.AlcanceRedIP)? true:false;
+                vm.BlockTab3 = (3 <= vm.AlcanceRedIP)? true:false;
+                vm.BlockTab4 = (4 <= vm.AlcanceRedIP)? true:false;
+                vm.BlockTab5 = (5 <= vm.AlcanceRedIP)? true:false;
+                vm.TabActive = (vm.AlcanceRedIP > 0)? 0:5;
+                console.log(vm.AlcanceRedIP, vm.TabActive);
+            });
         }
 
         function SaveRedes(){
@@ -17,13 +28,25 @@ angular
                 var Red = data.GetDeepCatalogo_IpsResult;
                 if(Red != null){
                     vm.IdRed = Red.IdRed;
+                    vm.Mask = Red.mask;
+                    vm.Mod = 0;
                     var IPRed = DivIP(Red.IPRed);
                     vm.IP1 = parseInt(IPRed[0]);
                     vm.IP2 = parseInt(IPRed[1]);
                     vm.IP3 = parseInt(IPRed[2]);
                     vm.IP4 = parseInt(IPRed[3]);
-                    vm.Mask = Red.mask;
-                    vm.Mod = 0;
+                    GetRelRedDistribuidorDisList();
+                    GetRelRedDistribuidorIncList();
+                    GetRelRedPlazaDisList();
+                    GetRelRedPlazaIncList();
+                    GetRelRedEstadoDisList();
+                    GetRelRedEstadoIncList();
+                    GetRelRedCiudadDisList();
+                    GetRelRedCiudadIncList();
+                    GetRelRedLocalidadDisList();
+                    GetRelRedLocalidadIncList();
+                    GetRelRedMedioDisList();
+                    GetRelRedMedioIncList();
                 }else{
                     ngNotify.set('ERROR, No se encontró la Red seleccionada.', 'warn');
                     $state.go('home.catalogos.redes');
@@ -31,69 +54,187 @@ angular
             });
         }
 
+        function GetRelRedDistribuidorDisList(){
+            CatalogosRedIPFactory.GetRelRedPlazaList(vm.IdRed).then(function(data){
+                console.log(data);
+                vm.RelRedDistribuidorDisList = data.GetRelRedPlazaListResult;
+            });
+        }
+
+        function GetRelRedDistribuidorIncList(){
+            CatalogosRedIPFactory.GetRelRedPlaza_Inc(vm.IdRed).then(function(data){
+                console.log(data);
+                vm.RelRedDistribuidorIncList = data.GetRelRedPlaza_IncResult;
+            });
+        }
+
+        function UpdateRelRedDistribuidor(Op, Clv_Plaza){
+            var objRelRedPlaza = {
+                'IdRed': vm.IdRed,
+                'Clv_Plaza': Clv_Plaza,
+                'Op': Op
+            };
+            console.log(objRelRedPlaza);
+            CatalogosRedIPFactory.UpdateRelRedPlaza(objRelRedPlaza).then(function(data){
+                console.log(data);
+                GetRelRedDistribuidorDisList();
+                GetRelRedDistribuidorIncList();
+            });
+        }
+
+        function GetRelRedPlazaDisList(){
+            CatalogosRedIPFactory.GetRelRedCompaniaList(vm.IdRed).then(function(data){
+                console.log(data);
+                vm.RelRedPlazaDisList = data.GetRelRedCompaniaListResult;
+            });
+        }
+
+        function GetRelRedPlazaIncList(){
+            CatalogosRedIPFactory.GetRelRedCompania_Inc(vm.IdRed).then(function(data){
+                console.log(data);
+                vm.RelRedPlazaIncList = data.GetRelRedCompania_IncResult;
+            });
+        }
+
+        function UpdateRelRedPlaza(Op, IdCompania){
+            var objRelRedCompania = {
+                'IdRed': vm.IdRed,
+                'IdCompania': IdCompania,
+                'Op': Op
+            };
+            CatalogosRedIPFactory.UpdateRelRedCompania(objRelRedCompania).then(function(data){
+                console.log(data);
+                GetRelRedPlazaDisList();
+                GetRelRedPlazaIncList();
+            });
+        }
+
+        
+        function GetRelRedEstadoDisList(){
+            CatalogosRedIPFactory.GetRelRedEstado_Dis(vm.IdRed).then(function(data){
+                console.log(data);
+                vm.RelRedEstadoDisList = data.GetRelRedEstado_DisResult;
+            });
+        }
+
+        function GetRelRedEstadoIncList(){
+            CatalogosRedIPFactory.GetRelRedEstadoList(vm.IdRed).then(function(data){
+                console.log(data);
+                vm.RelRedEstadoIncList = data.GetRelRedEstadoListResult;
+            });
+        }
+
+        function UpdateRelRedEstado(Op, Clv_Estado){
+            var objRelRedEstado = {
+                'IdRed': vm.IdRed,
+                'Clv_Estado': Clv_Estado,
+                'Op': Op
+            };
+            CatalogosRedIPFactory.UpdateRelRedEstado(objRelRedEstado).then(function(data){
+                console.log(data);
+                GetRelRedEstadoDisList();
+                GetRelRedEstadoIncList();
+            });
+        }
+
+        
+        function GetRelRedCiudadDisList(){
+            CatalogosRedIPFactory.GetRelRedCiudad_Dis(vm.IdRed).then(function(data){
+                console.log(data);
+                vm.RelRedCiudadDisList = data.GetRelRedCiudad_DisResult;
+            });
+        }
+
+        function GetRelRedCiudadIncList(){
+            CatalogosRedIPFactory.GetRelRedCiudadList(vm.IdRed).then(function(data){
+                console.log(data);
+                vm.RelRedCiudadIncList = data.GetRelRedCiudadListResult;
+            });
+        }
+
+        function UpdateRelRedCiudad(Op, Clv_Ciudad){
+            var objRelRedCiudad = {
+                'IdRed': vm.IdRed,
+                'Clv_Ciudad': Clv_Ciudad,
+                'Op': Op
+            };
+            CatalogosRedIPFactory.UpdateRelRedCiudad(objRelRedCiudad).then(function(data){
+                console.log(data);
+                GetRelRedCiudadDisList();
+                GetRelRedCiudadIncList();
+            });
+        }
+        
+        
+        function GetRelRedLocalidadDisList(){
+            CatalogosRedIPFactory.GetRelRedLocalidadList(vm.IdRed).then(function(data){
+                console.log(data);
+                vm.RelRedLocalidadDisList = data.GetRelRedLocalidadListResult;
+            });
+        }
+        
+        function GetRelRedLocalidadIncList(){
+            CatalogosRedIPFactory.GetRelRedLocalidad_inc(vm.IdRed).then(function(data){
+                console.log(data);
+                vm.RelRedLocalidadIncList = data.GetRelRedLocalidad_incResult;
+            });
+        }
+
+        function UpdateRelRedLocalidad(Op, Clv_Localidad){
+            var objRelRedLocalidad = {
+                'IdRed': vm.IdRed,
+                'Clv_Localidad': Clv_Localidad,
+                'Op': Op
+            };
+            CatalogosRedIPFactory.UpdateRelRedLocalidad(objRelRedLocalidad).then(function(data){
+                console.log(data);
+                GetRelRedLocalidadDisList();
+                GetRelRedLocalidadIncList();
+            });
+        }
+
+        function GetRelRedMedioDisList(){
+            CatalogosRedIPFactory.GetRelRedMedioList(vm.IdRed).then(function(data){
+                console.log(data);
+                vm.RelRedMedioDisList = data.GetRelRedMedioListResult;
+            });
+        }
+
+        function GetRelRedMedioIncList(){
+            CatalogosRedIPFactory.GetRelRedMedio_Inc(vm.IdRed).then(function(data){
+                console.log(data);
+                vm.RelRedMedioIncList = data.GetRelRedMedio_IncResult;
+            });
+        }
+
+         function UpdateRelRedMedio(Op, IdMedio){
+             var objRelRedMedio = {
+                'IdRed': vm.IdRed,
+                'IdMedio': IdMedio,
+                'Op': Op
+             };
+            CatalogosRedIPFactory.UpdateRelRedMedio(objRelRedMedio).then(function(data){
+                console.log(data);
+                GetRelRedMedioDisList();
+                GetRelRedMedioIncList();
+            });
+         }        
+                    
         function DivIP(IP) {
             var PartIP = IP.split(".");
             return PartIP;
         }
 
-        function transfer(from, to, index) {
-            if (index >= 0) {
-                to.push(from[index]);
-                from.splice(index, 1);
-            } else {
-                for (var i = 0; i < from.length; i++) {
-                    to.push(from[i]);
-                }
-                from.length = 0;
-            }
-        }
-
         var vm = this;
-        vm.SelctItems = [];
-        vm.Items = [
-            {
-                Name: 'ABC',
-                Desc: 'abc'
-            },
-            {
-                Name: 'DEF',
-                Desc: 'def'
-            },
-            {   
-                Name: 'GHI',
-                Desc: 'ghi'
-            },
-            {
-                Name: 'JKllL',
-                Desc: 'jkl'
-            },
-            {
-                Name: 'ABC',
-                Desc: 'abc'
-            },
-            {
-                Name: 'DEF',
-                Desc: 'def'
-            },
-            {   
-                Name: 'GHI',
-                Desc: 'ghi'
-            },
-            {
-                Name: 'JKllL',
-                Desc: 'jkl'
-            }
-        ];
-        vm.SaveRedes = SaveRedes;
-        vm.transfer = transfer;
         vm.Block = true;
-        vm.AvanceRedIP = 1;
-        vm.BlockTab1 = (1 <= vm.AvanceRedIP)? true:false;
-        vm.BlockTab2 = (2 <= vm.AvanceRedIP)? true:false;
-        vm.BlockTab3 = (3 <= vm.AvanceRedIP)? true:false;
-        vm.BlockTab4 = (4 <= vm.AvanceRedIP)? true:false;
-        vm.BlockTab5 = (5 <= vm.AvanceRedIP)? true:false;
-        vm.TabActive = (vm.AvanceRedIP > 0)? 0:5;
+        vm.TabActive = 0;
+        vm.SaveRedes = SaveRedes;
+        vm.UpdateRelRedDistribuidor = UpdateRelRedDistribuidor;
+        vm.UpdateRelRedPlaza = UpdateRelRedPlaza;
+        vm.UpdateRelRedEstado = UpdateRelRedEstado;
+        vm.UpdateRelRedCiudad = UpdateRelRedCiudad;
+        vm.UpdateRelRedLocalidad = UpdateRelRedLocalidad;
+        vm.UpdateRelRedMedio = UpdateRelRedMedio;
         initData();
-        
+
     });
--- app/scripts/factories/CatalogosRedIPFactory.js
+++ app/scripts/factories/CatalogosRedIPFactory.js
@@ -13,7 +13,26 @@ angular
             GetcatalogoIps_dosList: '/catalogoIps_dos/GetcatalogoIps_dosList',
             UpdatecatalogoIps_dos: '/catalogoIps_dos/UpdatecatalogoIps_dos',
             GetDeepcatalogoIps_dos: '/catalogoIps_dos/GetDeepcatalogoIps_dos',
-            GetListCombo: '/catalogoIps_dos/GetListCombo'
+            GetListCombo: '/catalogoIps_dos/GetListCombo',
+            GetGeneral_Sistema_II: '/General_Sistema_II/GetGeneral_Sistema_II',
+            GetRelRedPlazaList: '/RelRedPlaza/GetRelRedPlazaList',
+            GetRelRedPlaza_Inc: '/RelRedPlaza/GetRelRedPlaza_Inc',
+            UpdateRelRedPlaza: '/RelRedPlaza/UpdateRelRedPlaza',
+            GetRelRedCompaniaList: '/RelRedCompania/GetRelRedCompaniaList',
+            GetRelRedCompania_Inc: '/RelRedCompania/GetRelRedCompania_Inc',
+            UpdateRelRedCompania: '/RelRedCompania/UpdateRelRedCompania',
+            GetRelRedEstado_Dis: '/RelRedEstado/GetRelRedEstado_Dis',
+            GetRelRedEstadoList: '/RelRedEstado/GetRelRedEstadoList',
+            UpdateRelRedEstado: '/RelRedEstado/UpdateRelRedEstado',
+            GetRelRedCiudad_Dis: '/RelRedCiudad/GetRelRedCiudad_Dis',
+            GetRelRedCiudadList: '/RelRedCiudad/GetRelRedCiudadList',
+            UpdateRelRedCiudad: '/RelRedCiudad/UpdateRelRedCiudad',
+            GetRelRedLocalidadList: '/RelRedLocalidad/GetRelRedLocalidadList',
+            GetRelRedLocalidad_inc: '/RelRedLocalidad/GetRelRedLocalidad_inc',
+            UpdateRelRedLocalidad: '/RelRedLocalidad/UpdateRelRedLocalidad',
+            GetRelRedMedioList: '/RelRedMedio/GetRelRedMedioList',
+            GetRelRedMedio_Inc: '/RelRedMedio/GetRelRedMedio_Inc',
+            UpdateRelRedMedio: '/RelRedMedio/UpdateRelRedMedio'
         };
 
         factory.GetCatalogo_IpsList = function (ObjRedIP) {
@@ -92,6 +111,245 @@ angular
             var deferred = $q.defer();
             var config = {headers: {'Authorization': $localStorage.currentUser.token}};
             $http.get(globalService.getUrl() + paths.GetListCombo, config).then(function (response) {
+                deferred.resolve(response.data);
+            }).catch(function (response) {
+                deferred.reject(response);
+            });
+            return deferred.promise;
+        };
+
+        factory.GetGeneral_Sistema_II = function () {
+            var deferred = $q.defer();
+            var config = {headers: {'Authorization': $localStorage.currentUser.token}};
+            $http.get(globalService.getUrl() + paths.GetGeneral_Sistema_II, config).then(function (response) {
+                deferred.resolve(response.data);
+            }).catch(function (response) {
+                deferred.reject(response);
+            });
+            return deferred.promise;
+        };
+
+        factory.GetRelRedPlazaList = function (IdRed) {
+            var deferred = $q.defer();
+            var config = {headers: {'Authorization': $localStorage.currentUser.token}};
+            var Parametros = {'IdRed': IdRed};
+            $http.post(globalService.getUrl() + paths.GetRelRedPlazaList, JSON.stringify(Parametros), config).then(function (response) {
+                deferred.resolve(response.data);
+            }).catch(function (response) {
+                deferred.reject(response);
+            });
+            return deferred.promise;
+        };
+
+        factory.GetRelRedPlaza_Inc = function (IdRed) {
+            var deferred = $q.defer();
+            var config = {headers: {'Authorization': $localStorage.currentUser.token}};
+            var Parametros = {'IdRed': IdRed};
+            console.log(Parametros);
+            $http.post(globalService.getUrl() + paths.GetRelRedPlaza_Inc, JSON.stringify(Parametros), config).then(function (response) {
+                deferred.resolve(response.data);
+            }).catch(function (response) {
+                deferred.reject(response);
+            });
+            return deferred.promise;
+        };
+
+        factory.UpdateRelRedPlaza = function (objRelRedPlaza) {
+            var deferred = $q.defer();
+            var config = {headers: {'Authorization': $localStorage.currentUser.token}};
+            var Parametros = {'objRelRedPlaza': objRelRedPlaza};
+            console.log(Parametros);
+            $http.post(globalService.getUrl() + paths.UpdateRelRedPlaza, JSON.stringify(Parametros), config).then(function (response) {
+                deferred.resolve(response.data);
+            }).catch(function (response) {
+                deferred.reject(response);
+            });
+            return deferred.promise;
+        };
+
+        factory.GetRelRedCompaniaList = function (IdRed) {
+            var deferred = $q.defer();
+            var config = {headers: {'Authorization': $localStorage.currentUser.token}};
+            var Parametros = {'IdRed': IdRed};
+            $http.post(globalService.getUrl() + paths.GetRelRedCompaniaList, JSON.stringify(Parametros), config).then(function (response) {
+                deferred.resolve(response.data);
+            }).catch(function (response) {
+                deferred.reject(response);
+            });
+            return deferred.promise;
+        };
+
+        factory.GetRelRedCompania_Inc = function (IdRed) {
+            var deferred = $q.defer();
+            var config = {headers: {'Authorization': $localStorage.currentUser.token}};
+            var Parametros = {'IdRed': IdRed};
+            console.log(Parametros);
+            $http.post(globalService.getUrl() + paths.GetRelRedCompania_Inc, JSON.stringify(Parametros), config).then(function (response) {
+                deferred.resolve(response.data);
+            }).catch(function (response) {
+                deferred.reject(response);
+            });
+            return deferred.promise;
+        };
+
+        factory.UpdateRelRedCompania = function (objRelRedCompania) {
+            var deferred = $q.defer();
+            var config = {headers: {'Authorization': $localStorage.currentUser.token}};
+            var Parametros = {'objRelRedCompania': objRelRedCompania};
+            console.log(Parametros);
+            $http.post(globalService.getUrl() + paths.UpdateRelRedCompania, JSON.stringify(Parametros), config).then(function (response) {
+                deferred.resolve(response.data);
+            }).catch(function (response) {
+                deferred.reject(response);
+            });
+            return deferred.promise;
+        };
+
+        factory.GetRelRedEstado_Dis = function (IdRed) {
+            var deferred = $q.defer();
+            var config = {headers: {'Authorization': $localStorage.currentUser.token}};
+            var Parametros = {'IdRed': IdRed};
+            $http.post(globalService.getUrl() + paths.GetRelRedEstado_Dis, JSON.stringify(Parametros), config).then(function (response) {
+                deferred.resolve(response.data);
+            }).catch(function (response) {
+                deferred.reject(response);
+            });
+            return deferred.promise;
+        };
+
+        factory.GetRelRedEstadoList = function (IdRed) {
+            var deferred = $q.defer();
+            var config = {headers: {'Authorization': $localStorage.currentUser.token}};
+            var Parametros = {'IdRed': IdRed};
+            console.log(Parametros);
+            $http.post(globalService.getUrl() + paths.GetRelRedEstadoList, JSON.stringify(Parametros), config).then(function (response) {
+                deferred.resolve(response.data);
+            }).catch(function (response) {
+                deferred.reject(response);
+            });
+            return deferred.promise;
+        };
+
+        factory.UpdateRelRedEstado = function (objRelRedEstado) {
+            var deferred = $q.defer();
+            var config = {headers: {'Authorization': $localStorage.currentUser.token}};
+            var Parametros = {'objRelRedEstado': objRelRedEstado};
+            console.log(Parametros);
+            $http.post(globalService.getUrl() + paths.UpdateRelRedEstado, JSON.stringify(Parametros), config).then(function (response) {
+                deferred.resolve(response.data);
+            }).catch(function (response) {
+                deferred.reject(response);
+            });
+            return deferred.promise;
+        };
+
+        factory.GetRelRedCiudad_Dis = function (IdRed) {
+            var deferred = $q.defer();
+            var config = {headers: {'Authorization': $localStorage.currentUser.token}};
+            var Parametros = {'IdRed': IdRed};
+            $http.post(globalService.getUrl() + paths.GetRelRedCiudad_Dis, JSON.stringify(Parametros), config).then(function (response) {
+                deferred.resolve(response.data);
+            }).catch(function (response) {
+                deferred.reject(response);
+            });
+            return deferred.promise;
+        };
+
+        factory.GetRelRedCiudadList = function (IdRed) {
+            var deferred = $q.defer();
+            var config = {headers: {'Authorization': $localStorage.currentUser.token}};
+            var Parametros = {'IdRed': IdRed};
+            console.log(Parametros);
+            $http.post(globalService.getUrl() + paths.GetRelRedCiudadList, JSON.stringify(Parametros), config).then(function (response) {
+                deferred.resolve(response.data);
+            }).catch(function (response) {
+                deferred.reject(response);
+            });
+            return deferred.promise;
+        };
+
+        factory.UpdateRelRedCiudad = function (objRelRedCiudad) {
+            var deferred = $q.defer();
+            var config = {headers: {'Authorization': $localStorage.currentUser.token}};
+            var Parametros = {'objRelRedCiudad': objRelRedCiudad};
+            console.log(Parametros);
+            $http.post(globalService.getUrl() + paths.UpdateRelRedCiudad, JSON.stringify(Parametros), config).then(function (response) {
+                deferred.resolve(response.data);
+            }).catch(function (response) {
+                deferred.reject(response);
+            });
+            return deferred.promise;
+        };
+
+        factory.GetRelRedLocalidadList = function (IdRed) {
+            var deferred = $q.defer();
+            var config = {headers: {'Authorization': $localStorage.currentUser.token}};
+            var Parametros = {'IdRed': IdRed};
+            $http.post(globalService.getUrl() + paths.GetRelRedLocalidadList, JSON.stringify(Parametros), config).then(function (response) {
+                deferred.resolve(response.data);
+            }).catch(function (response) {
+                deferred.reject(response);
+            });
+            return deferred.promise;
+        };
+
+        factory.GetRelRedLocalidad_inc = function (IdRed) {
+            var deferred = $q.defer();
+            var config = {headers: {'Authorization': $localStorage.currentUser.token}};
+            var Parametros = {'IdRed': IdRed};
+            console.log(Parametros);
+            $http.post(globalService.getUrl() + paths.GetRelRedLocalidad_inc, JSON.stringify(Parametros), config).then(function (response) {
+                deferred.resolve(response.data);
+            }).catch(function (response) {
+                deferred.reject(response);
+            });
+            return deferred.promise;
+        };
+
+        factory.UpdateRelRedLocalidad = function (objRelRedLocalidad) {
+            var deferred = $q.defer();
+            var config = {headers: {'Authorization': $localStorage.currentUser.token}};
+            var Parametros = {'objRelRedLocalidad': objRelRedLocalidad};
+            console.log(Parametros);
+            $http.post(globalService.getUrl() + paths.UpdateRelRedLocalidad, JSON.stringify(Parametros), config).then(function (response) {
+                deferred.resolve(response.data);
+            }).catch(function (response) {
+                deferred.reject(response);
+            });
+            return deferred.promise;
+        };
+
+        factory.GetRelRedMedioList = function (IdRed) {
+            var deferred = $q.defer();
+            var config = {headers: {'Authorization': $localStorage.currentUser.token}};
+            var Parametros = {'IdRed': IdRed};
+            $http.post(globalService.getUrl() + paths.GetRelRedMedioList, JSON.stringify(Parametros), config).then(function (response) {
+                deferred.resolve(response.data);
+            }).catch(function (response) {
+                deferred.reject(response);
+            });
+            return deferred.promise;
+        };
+
+        factory.GetRelRedMedio_Inc = function (IdRed) {
+            var deferred = $q.defer();
+            var config = {headers: {'Authorization': $localStorage.currentUser.token}};
+            var Parametros = {'IdRed': IdRed};
+            console.log(Parametros);
+            $http.post(globalService.getUrl() + paths.GetRelRedMedio_Inc, JSON.stringify(Parametros), config).then(function (response) {
+                deferred.resolve(response.data);
+            }).catch(function (response) {
+                deferred.reject(response);
+            });
+            return deferred.promise;
+        };
+
+        factory.UpdateRelRedMedio = function (objRelRedMedio) {
+            var deferred = $q.defer();
+            var config = {headers: {'Authorization': $localStorage.currentUser.token}};
+            var Parametros = {'objRelRedMedio': objRelRedMedio};
+            console.log(Parametros);
+            $http.post(globalService.getUrl() + paths.UpdateRelRedMedio, JSON.stringify(Parametros), config).then(function (response) {
                 deferred.resolve(response.data);
             }).catch(function (response) {
                 deferred.reject(response);
