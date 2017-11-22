110888e2c0b4bdb6836c8626c84581930bf2892e
Unknown <alejandro_jimenez24@hotmail.com>
2017-11-22 09:46:34 -0600

    Vendedores - ventanas

---
+++ app/scripts/controllers/ventas/ModalVendedorUpdateCtrl.js
@@ -0,0 +1,14 @@
+'use strict';
+
+angular
+    .module('softvApp')
+    .controller('ModalVendedorUpdateCtrl', function(VentasFactory, $uibModalInstance, $uibModal, ngNotify, $state, $rootScope){
+        
+        function cancel() {
+            $uibModalInstance.dismiss('cancel');
+        }
+
+        var vm = this;
+        vm.cancel = cancel;
+        
+    });
