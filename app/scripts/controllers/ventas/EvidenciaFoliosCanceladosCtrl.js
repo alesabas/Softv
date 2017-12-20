532b90fc1f68994df075259411171efacff77bbc
Unknown <alejandro_jimenez24@hotmail.com>
2017-12-20 17:33:59 -0600

    Evidencia Folios Cancelados

--- app/scripts/controllers/ventas/EvidenciaFoliosCanceladosCtrl.js
+++ app/scripts/controllers/ventas/EvidenciaFoliosCanceladosCtrl.js
@@ -2,7 +2,7 @@
 
 angular
     .module('softvApp')
-    .controller('EvidenciaFoliosCanceladosCtrl', function(SeriesFactory, ngNotify, $uibModal, $rootScope, $state, $localStorage){
+    .controller('EvidenciaFoliosCanceladosCtrl', function(SeriesFactory, ngNotify, $uibModal, $rootScope, $state, $localStorage, globalService, $sce){
         
         function initData(){
             GetFolioCanceladoList(0);
@@ -29,8 +29,21 @@ angular
                 'clv_vendedor': Evidencia.clv_vendedor
             };
             SeriesFactory.GetDameTipoEvidencia(ObjTipoEvidencia).then(function(data){
-                SeriesFactory.GetEvidenciaSerieFolioVEndedor(ObjTipoEvidencia).then(function(data){
+                console.log(data);
+                var TipoDocumento = data.GetDameTipoEvidenciaResult.tipoDocumento;
+                vm.ShowDoc = (TipoDocumento == 1)? true:false;
+                var ObjEvidencia = {
+                    'serie': Evidencia.serie,
+                    'folio': Evidencia.folio,
+                    'clv_vendedor': Evidencia.clv_vendedor,
+                    'tipoDocumento': TipoDocumento
+                };
+                SeriesFactory.GetEvidenciaSerieFolioVEndedor(ObjEvidencia).then(function(data){
                     console.log(data);
+                    var Name = data.GetEvidenciaSerieFolioVEndedorResult;
+                    var FileName = globalService.getUrlReportes() + '/Images/' + Name;
+                    //vm.FileName = $sce.trustAsResourceUrl(vm.f);
+                    vm.FileName = $sce.trustAsResourceUrl(FileName);
                 });
             });
         }
@@ -41,6 +54,7 @@ angular
 
         var vm = this;
         vm.View = false;
+        vm.ShowDoc = true;
         vm.GetFolioCanceladoList = GetFolioCanceladoList;
         vm.GetEvidencia = GetEvidencia;
         vm.cancel = cancel;
