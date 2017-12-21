a8d3d6f04ba3ed8fb4598b806f59820d2818ea9e
EricChavez <erictics@gmail.com>
2017-12-21 16:56:12 -0600

    integracion de catalogo prueba de internet

--- app/scripts/factories/procesoFactory.js
+++ app/scripts/factories/procesoFactory.js
@@ -5,10 +5,58 @@ angular
 
     var paths = {
         GetMuestraServCteReset:'/Procesos/GetMuestraServCteReset',
-        GetResetServCte:'/Procesos/GetResetServCte'
+        GetResetServCte:'/Procesos/GetResetServCte',
+        GetMuestraServiciosPrueba:'/Procesos/GetMuestraServiciosPrueba',
+        GetMUESTRACablemodesDelClientePrueba:'/Procesos/GetMUESTRACablemodesDelClientePrueba'
     };
 
     var factory = {};   
+
+    factory.GetMUESTRACablemodesDelClientePrueba = function (contrato) {
+      var deferred = $q.defer();
+      var config = {
+        headers: {
+          'Authorization': $localStorage.currentUser.token
+        }
+      };
+      var Parametros = {
+        'contrato': contrato
+      
+      };
+
+      $http.post(globalService.getUrl() + paths.GetMUESTRACablemodesDelClientePrueba, JSON.stringify(Parametros), config).then(function (response) {
+        deferred.resolve(response.data);
+      }).catch(function (response) {
+        deferred.reject(response);
+      });
+      return deferred.promise;
+    };
+
+
+
+    factory.GetMuestraServiciosPrueba = function (Clv_TipSer,Clv_Servicio,Clv_Unicanet) {
+      var deferred = $q.defer();
+      var config = {
+        headers: {
+          'Authorization': $localStorage.currentUser.token
+        }
+      };
+      var Parametros = {
+        'Clv_TipSer': Clv_TipSer,
+        'Clv_Servicio':Clv_Servicio,
+        'Clv_Unicanet':Clv_Unicanet      
+      };
+
+      $http.post(globalService.getUrl() + paths.GetMuestraServiciosPrueba, JSON.stringify(Parametros), config).then(function (response) {
+        deferred.resolve(response.data);
+      }).catch(function (response) {
+        deferred.reject(response);
+      });
+      return deferred.promise;
+    };
+
+
+
 
     factory.GetMuestraServCteReset = function (Contrato,Clv_TipSer,idcompania) {
         var deferred = $q.defer();
